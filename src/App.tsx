2de7ccb64048d733ca5cad0a2ed349fef259fe66
Guenther <guenther.zoppelt@gmail.com>
2019-09-30 05:40:28 +0100

    end_46

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-const App: React.FC = () => {
+const App_asFunction: React.FC = () => {
   const [name, setName] = useState('Guenther')
   const [age, setAge] = useState(28)
   const [text, setText] = useState('and I am not stupid')
@@ -27,6 +27,68 @@ const App: React.FC = () => {
         <button onClick={switchNameHandler}>Switch Name</button>
     </div>
   );
+}
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28, clever: true  },
+      { name: 'Manu', age: 29, clever: true  },
+      { name: 'Stephanie', age: 26, clever: false  },
+    ],
+    otherState: 'some other value',
+  }
+
+  handleNameSwitch = (newName: string) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 28, clever: true },
+        { name: 'Manu', age: 29, clever: true },
+        { name: 'Stephanie', age: 26, clever: false },
+      ]
+    })
+  }
+
+  handleNameChange = (event: any) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28, clever: true },
+        { name: event.target.value, age: 29, clever: true },
+        { name: 'Stephanie', age: 26, clever: false },
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React App</h1>
+        <p>
+          <button onClick={this.handleNameSwitch.bind(this, 'Maximilian')}>Switch Name</button>
+        </p>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          clever={this.state.persons[0].clever}
+          click={this.handleNameSwitch.bind(this, 'Max')}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          clever={this.state.persons[1].clever}
+          change={this.handleNameChange}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          clever={this.state.persons[2].clever}
+        />
+      </div>
+    )
+  }
+
 }
 
 export default App;
--- src/Person/Person.tsx
+++ src/Person/Person.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 
 
-const Person = (props: any) => (
-    <div>
-    <p onClick={props.click}>
-        I am {props.name} and I am {props.age} years old.<br />
-        <i>{props.children}</i>
-    </p>
-    
-    </div>
-)
-
-// {
-//     return (
-//         <p>I am Person!</p>
-//     )
-// }
+const Person = (props: any) => {
+    const clever = props.clever ? 'Yes' : 'No'
+    return (
+        <div onClick={props.click}>
+            <p>
+                I am {props.name} and I am {props.age} years old.<br />
+                <i> clever: {clever}</i>
+                <br />
+                <input type="text" onChange={props.change} value={props.name} />
+            </p>
+           
+        </div>
+    )
+}
    
 export default Person
+
